5d4e100527be009e82e81e8dfb750c0282af6b5b
Atos Vinicius <athos.vinicius.pereira@gmail.com>
2026-03-05 15:26:15 -0300

    update

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = 3001;
+const PORT = 3500;
 
 app.use(cors());
 app.use(express.json({ limit: "50mb" })); // Increased limit for base64 images
